0aad59fcf7dc0ce91410cf4b2ad9eaf8284e5050
galawoj <125212314+galawoj@users.noreply.github.com>
2023-02-24 22:50:31 +0100

    Add files via upload

---
+++ gra2.js
@@ -0,0 +1,458 @@
+let los = "babka";
+los = los + los
+let wier = 0;
+let slowo
+
+
+
+
+function enter() {
+    let kolory = []
+    let los_tabl = []
+    if (document.getElementById(`${wier},4`).textContent != "") {
+
+
+        slowo = [
+            document.getElementById(`${wier},0`).textContent,
+            document.getElementById(`${wier},1`).textContent,
+            document.getElementById(`${wier},2`).textContent,
+            document.getElementById(`${wier},3`).textContent,
+            document.getElementById(`${wier},4`).textContent
+        ]
+
+
+
+
+        for (let i = 0; i < los.length; i++) {
+            los_tabl.push(los.charAt(i))
+        }
+
+        for (i = 0; i < 5; i++) {
+            if (slowo[i] == los_tabl[i]) {
+                kolory.push("orange")
+                los_tabl.splice(i, 1, "")
+                los_tabl.splice(i + 5, 1, "")
+
+            } else {
+                kolory.push("gray")
+            }
+        }
+        for (i = 0; i < 5; i++) {
+
+            if (slowo[i] == los_tabl[i + 1]) {
+                if (kolory[i] != "orange") { kolory.splice(i, 1, "lightblue") }
+            } else if (slowo[i] == los_tabl[i + 2]) {
+                if (kolory[i] != "orange") { kolory.splice(i, 1, "lightblue") }
+            } else if (slowo[i] == los_tabl[i + 3]) {
+                if (kolory[i] != "orange") { kolory.splice(i, 1, "lightblue") }
+            } else if (slowo[i] == los_tabl[i + 4]) {
+                if (kolory[i] != "orange") { kolory.splice(i, 1, "lightblue") }
+            }
+        }
+
+        document.getElementById(`${wier},0`).style.backgroundColor = kolory[0]
+        document.getElementById(`${wier},1`).style.backgroundColor = kolory[1]
+        document.getElementById(`${wier},2`).style.backgroundColor = kolory[2]
+        document.getElementById(`${wier},3`).style.backgroundColor = kolory[3]
+        document.getElementById(`${wier},4`).style.backgroundColor = kolory[4]
+        
+       
+        
+        if(kolory[0] + kolory[1] + kolory[2] + kolory[3] + kolory[4] == "orangeorangeorangeorangeorange") {
+            document.getElementById("winner").innerHTML = "WYGRAŁEŚ BABECZKĘ!!!"
+            wier =6
+        } else {
+            wier = wier + 1
+        }
+
+    }
+}
+
+function backspace() {
+    let kol = 0
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        if (kol == 4) break;
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+    if (document.getElementById(`${wier},${4}`).textContent != "") {
+        document.getElementById(`${wier},${4}`).innerHTML = "";
+    } else {
+        document.getElementById(`${wier},${kol - 1}`).innerHTML = "";
+    }
+}
+function a() {
+    let kol = 0
+    let litera = document.getElementById("a").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function b() {
+    let kol = 0;
+    let litera = document.getElementById("b").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function c() {
+    let kol = 0;
+    let litera = document.getElementById("c").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function d() {
+    let kol = 0;
+    let litera = document.getElementById("d").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function e() {
+    let kol = 0;
+    let litera = document.getElementById("e").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function f() {
+    let kol = 0;
+    let litera = document.getElementById("f").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function g() {
+    let kol = 0;
+    let litera = document.getElementById("g").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function h() {
+    let kol = 0;
+    let litera = document.getElementById("h").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function ii() {
+    let kol = 0;
+    let litera = document.getElementById("i").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function j() {
+    let kol = 0;
+    let litera = document.getElementById("j").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function k() {
+    let kol = 0;
+    let litera = document.getElementById("k").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function l() {
+    let kol = 0;
+    let litera = document.getElementById("l").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function m() {
+    let kol = 0;
+    let litera = document.getElementById("m").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function n() {
+    let kol = 0;
+    let litera = document.getElementById("n").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function o() {
+    let kol = 0;
+    let litera = document.getElementById("o").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function q() {
+    let kol = 0;
+    let litera = document.getElementById("q").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function p() {
+    let kol = 0;
+    let litera = document.getElementById("p").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function r() {
+    let kol = 0;
+    let litera = document.getElementById("r").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function s() {
+    let kol = 0;
+    let litera = document.getElementById("s").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function t() {
+    let kol = 0;
+    let litera = document.getElementById("t").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function u() {
+    let kol = 0;
+    let litera = document.getElementById("u").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function v() {
+    let kol = 0;
+    let litera = document.getElementById("v").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function w() {
+    let kol = 0;
+    let litera = document.getElementById("w").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function x() {
+    let kol = 0;
+    let litera = document.getElementById("x").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function y() {
+    let kol = 0;
+    let litera = document.getElementById("y").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function z() {
+    let kol = 0;
+    let litera = document.getElementById("z").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function aa() {
+    let kol = 0;
+    let litera = document.getElementById("aa").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function cc() {
+    let kol = 0;
+    let litera = document.getElementById("cc").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function ee() {
+    let kol = 0;
+    let litera = document.getElementById("ee").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function oo() {
+    let kol = 0;
+    let litera = document.getElementById("oo").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function ll() {
+    let kol = 0;
+    let litera = document.getElementById("ll").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function ss() {
+    let kol = 0;
+    let litera = document.getElementById("ss").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function zz() {
+    let kol = 0;
+    let litera = document.getElementById("zz").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
+function zzz() {
+    let kol = 0;
+    let litera = document.getElementById("zzz").textContent;
+    let litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    while (litera2 != "") {
+        kol++
+        litera2 = document.getElementById(`${wier},${kol}`).textContent;
+    }
+
+    document.getElementById(`${wier},${kol}`).innerHTML = litera;
+}
